25674dbf2c9426a751c3d80470f3053fc4f4ee87
Arsalan Sadri <asadri62@gmail.com>
2021-01-18 12:10:15 -0800

    adding BASE to the tweet router

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -2,6 +2,7 @@ module.exports = {
   addOne: (ctx) => {
     return client.Car.create(ctx.request.body);
   },
+
   getAll: async (client) => {
     const { body } = await client.search({
       index: 'game-of-thrones',
@@ -14,10 +15,12 @@ module.exports = {
 
     return body.hits.hits;
   },
+
   deleteOne: ({ params }) => {
     const { make, model, year } = params;
     return client.Car.findOneAndDelete({ make, model, year });
   },
+
   updateOne: ({ params: { make, model, year }, request: { body } }) => {
     return client.Car.findOneAndUpdate({ make, model, year }, body);
   },
--- routes/tweet.js
+++ routes/tweet.js
@@ -1,19 +1,21 @@
 const router = require('koa-router')();
 const tweetController = require('../controllers/tweetController.js');
 
+const BASE = '/api/tweets';
+
 router
-  .post('/api/tweets', async (ctx) => {
+  .post(`${BASE}`, async (ctx) => {
     const dbCar = await tweetController.addOne(ctx);
     ctx.body = JSON.stringify(dbCar);
   })
-  .get('/api/tweets', async (ctx) => {
+  .get(`${BASE}`, async (ctx) => {
     ctx.body = await tweetController.getAll(ctx.app.client);
   })
-  .delete('/api/tweets/:make/:model/:year', async (ctx, next) => {
+  .delete(`${BASE}/:make/:model/:year`, async (ctx, next) => {
     await tweetController.deleteOne(ctx);
     ctx.status = 200;
   })
-  .put('/api/tweets/:make/:model/:year', async (ctx, next) => {
+  .put(`${BASE}/:make/:model/:year`, async (ctx, next) => {
     await tweetController.updateOne(ctx);
     ctx.status = 200;
   });
