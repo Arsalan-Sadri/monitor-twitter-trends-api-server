b7ff57a4a836a93c01afc5d943ea7a1c31ca67cb
Arsalan Sadri <asadri62@gmail.com>
2021-01-18 13:15:14 -0800

    logging success result of index creation

--- mappings/twitter_index.js
+++ mappings/twitter_index.js
@@ -1,32 +1,40 @@
 const { client } = require('../config.js');
 
-client.indices.create({
-  index: 'twitter',
-  body: {
-    mappings: {
-      properties: {
-        type: {
-          type: 'keyword',
-        },
-        name: {
-          type: 'text',
-        },
-        username: {
-          type: 'keyword',
-        },
-        email: {
-          type: 'keyword',
-        },
-        text: {
-          type: 'text',
-        },
-        created_at: {
-          type: 'date',
-        },
-        author_id: {
-          type: 'keyword',
-        },
-      },
-    },
-  },
-});
+(async function run() {
+  // await client.indices.create({
+  //   index: 'twitter',
+  //   body: {
+  //     mappings: {
+  //       properties: {
+  //         type: {
+  //           type: 'keyword',
+  //         },
+  //         name: {
+  //           type: 'text',
+  //         },
+  //         username: {
+  //           type: 'keyword',
+  //         },
+  //         email: {
+  //           type: 'keyword',
+  //         },
+  //         text: {
+  //           type: 'text',
+  //         },
+  //         created_at: {
+  //           type: 'date',
+  //         },
+  //         author_id: {
+  //           type: 'keyword',
+  //         },
+  //       },
+  //     },
+  //   },
+  // });
+
+  const res = await client.indices.getMapping({
+    index: 'twitter',
+  });
+
+  if (res.statusCode === 200) console.log(res.body.twitter.mappings);
+})();
