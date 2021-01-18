edc22b2f86aea9b274fda705e44bd31a5f7238c4
Arsalan Sadri <asadri62@gmail.com>
2021-01-18 13:22:41 -0800

    formatting

--- routes/tweet.js
+++ routes/tweet.js
@@ -8,13 +8,16 @@ router
     const dbCar = await tweetController.insertOne(ctx);
     ctx.body = JSON.stringify(dbCar);
   })
+
   .get(`${BASE}`, async (ctx) => {
     ctx.body = await tweetController.getAll(ctx.app.client);
   })
+
   .delete(`${BASE}/:make/:model/:year`, async (ctx, next) => {
     await tweetController.deleteOne(ctx);
     ctx.status = 200;
   })
+
   .put(`${BASE}/:make/:model/:year`, async (ctx, next) => {
     await tweetController.updateOne(ctx);
     ctx.status = 200;
