042f8236742e717f9e8c45f9c9cf1d9cb93e4bce
Arsalan Sadri <asadri62@gmail.com>
2021-01-21 13:33:06 -0800

    redesigned config.js

--- config.js
+++ config.js
@@ -3,28 +3,24 @@ if (require.main.filename.includes('twitterIndex.js')) {
 }
 
 const { Client } = require('@elastic/elasticsearch');
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const {
+  NODE_ENV = 'development',
+  ELASTIC_CLOUD_ID: ID,
+  ELASTIC_CLOUD_USERNAME: USERNAME,
+  ELASTIC_CLOUD_PASSWORD: PASSWORD,
+} = process.env;
 
-if (NODE_ENV === 'development') {
-  module.exports = {
-    client: new Client({ node: 'http://localhost:9200' }),
-  };
-} else {
-  const {
-    ELASTIC_CLOUD_ID: ID,
-    ELASTIC_CLOUD_USERNAME: USERNAME,
-    ELASTIC_CLOUD_PASSWORD: PASSWORD,
-  } = process.env;
-
-  module.exports = {
-    client: new Client({
-      cloud: {
-        id: ID,
-      },
-      auth: {
-        username: USERNAME,
-        password: PASSWORD,
-      },
-    }),
-  };
-}
+module.exports = {
+  client:
+    NODE_ENV === 'development'
+      ? new Client({ node: 'http://localhost:9200' })
+      : new Client({
+          cloud: {
+            id: ID,
+          },
+          auth: {
+            username: USERNAME,
+            password: PASSWORD,
+          },
+        }),
+};
